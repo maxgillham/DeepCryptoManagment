1dca0b6677ecaaa7dd6bda5ba7db6661d0cba68e
maxgillham <maxfgillham@gmail.com>
2019-02-19 16:44:02 -0500

    rebuilt and organized eiie, no custom gradient yet

--- train.py
+++ train.py
@@ -16,14 +16,31 @@ class Portfolio():
         self.mini_batch_count = mini_batch_count
         self.epochs = epochs
 
-    def create_eiie(self, input_tensor_shape, rates_shape)
+    def create_eiie(self, input_tensor_shape, rates_shape):
         K.set_image_data_format("channels_first")
-        #3 inputs: main input, weight input, bias input
-        main_input = Sequential()
-        main_input.add(Conv2D(2, kernal_size=(3, 1), activation='relu',  input_shape=input_tensor_shape))
-        main_input.add(Conv2D(20, kernal_size=(48, 1), activation='relu'))
+        # 3 inputs: main input, weight input, bias input
+        main_input = Input(shape=input_tensor_shape, name='MainInput')
+        weight_input = Input(shape=rates_shape, name='WeightInput')
+        bias_input = Input(shape=(1, ), name='BiasInput')
 
+        # Assemble layers using the Functional API
+        # Starting with main input
+        x = Conv2D(2, kernel_size=(3, 1), activation='relu')(main_input)
+        x = Conv2D(20, kernel_size=(48, 1), activation='relu')(x)
+        # Adding the weight input. Note - A Lambda layer wraps a expression as a Layer object
+        # Here we call the expand_dimensions method defined in utils.py
+        weight_input_expression = Lambda(expand_dimensions)(weight_input)
+        x = Concatenate(axis=1)([x, weight_input_expression])
+        x = Conv2D(1, kernel_size=(1, 1))(x)
+        # Adding the bias input. Note - Same Lambda layer with expand_dimensions methods in utils.py
+        bias_input_expression = Lambda(expand_dimensions)(bias_input)
+        x = Concatenate(axis=3)([x, bias_input_expression])
+        model_output = Activation('softmax')(x)
 
+        # Inputs and output to create the ensemble
+        model = Model([main_input, weight_input, bias_input], model_output)
+        model.summary()
+        return
 
 
 if __name__ == "__main__":
