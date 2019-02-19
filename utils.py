43767f15ec5e80c33aea7e7d38deac0f1cc6dc2a
maxgillham <maxfgillham@gmail.com>
2019-02-19 16:43:12 -0500

    expand dimensions

--- utils.py
+++ utils.py
@@ -1,17 +1,15 @@
 import numpy as np
+import keras.backend as K
 
+# Load saved data for backtesting
 def load_data():
     x = np.load('./data/x.npy')
     y = np.load('./data/y.npy')
     rates = np.load('./data/rates.npy')
     return x, y, rates
 
-if __name__ == "__main__":
-    x, y, rates = load_data()
-    print(x.shape)
-    print(y.shape)
-    print(rates.shape)
-
-    print(x[0])
-    print(y[0])
-    print(rates[0])
+# Used in lambda layer
+def expand_dimensions(x):
+	expX = K.expand_dims(x, axis=1)
+	expX = K.expand_dims(expX, axis=1)
+	return expX
